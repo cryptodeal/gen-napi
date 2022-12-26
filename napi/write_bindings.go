6f2ff9f80da97f29f7351a5aa584bf21274a1b86
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-12-26 13:20:32 -0600

    fix import path for associated header when writing bindings

--- napi/write_bindings.go
+++ napi/write_bindings.go
@@ -2,14 +2,14 @@ package napi
 
 import (
 	"fmt"
-	"path"
+	"path/filepath"
 	"strings"
 )
 
 func (g *PackageGenerator) writeBindings(sb *strings.Builder, classes map[string]*CPPClass, methods map[string]*CPPMethod) {
 	// lower_caser := cases.Lower(language.AmericanEnglish)
 
-	sb.WriteString(fmt.Sprintf("#include %q\n", path.Base(g.conf.HeaderOutPath)))
+	sb.WriteString(fmt.Sprintf("#include %q\n", filepath.Base(g.conf.HeaderOutPath)))
 	g.writeBindingsFrontmatter(sb)
 	g.writeFileSourceHeader(sb, *g.Path)
 
