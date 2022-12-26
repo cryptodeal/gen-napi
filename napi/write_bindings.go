d34cf0f8435b1b4bed7c297f72793380aa1c6557
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-12-26 13:24:01 -0600

    adjust import path for gen header in bindings

--- napi/write_bindings.go
+++ napi/write_bindings.go
@@ -9,7 +9,7 @@ import (
 func (g *PackageGenerator) writeBindings(sb *strings.Builder, classes map[string]*CPPClass, methods map[string]*CPPMethod) {
 	// lower_caser := cases.Lower(language.AmericanEnglish)
 
-	sb.WriteString(fmt.Sprintf("#include %q\n", filepath.Base(g.conf.Path)))
+	sb.WriteString(fmt.Sprintf("#include %q\n", filepath.Base(g.conf.ResolvedHeaderOutPath(filepath.Dir(*g.Path)))))
 	g.writeBindingsFrontmatter(sb)
 	g.writeFileSourceHeader(sb, *g.Path)
 
