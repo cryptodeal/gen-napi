605b7cc3256261bc80c1e34f10e15275ffddf539
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-12-28 19:40:38 -0600

    attempt to fix `concat` by handling `std::vector<Tensor>` passed as arg

--- napi/write_bindings.go
+++ napi/write_bindings.go
@@ -31,6 +31,7 @@ func (g *PackageGenerator) writeMethod(sb *strings.Builder, m *CPPMethod, classe
 	lower_caser := cases.Lower(language.AmericanEnglish)
 	upper_caser := cases.Upper(language.AmericanEnglish)
 	parsedName := "_" + *m.Ident
+	namespace := ""
 	sb.WriteString(fmt.Sprintf("static Napi::Value %s(const Napi::CallbackInfo& info) {\n", parsedName))
 	g.writeIndent(sb, 1)
 	sb.WriteString("Napi::Env env = info.Env();\n")
@@ -112,6 +113,7 @@ func (g *PackageGenerator) writeMethod(sb *strings.Builder, m *CPPMethod, classe
 						sb.WriteString(";\n")
 					}
 				} else if isClass(*arg.Type, classes) {
+					namespace = *classes[*arg.Type].NameSpace
 					hasObject = true
 					g.writeIndent(sb, 1)
 					sb.WriteString(fmt.Sprintf("if (!info[%d].IsObject()) {\n", i))
@@ -143,6 +145,9 @@ func (g *PackageGenerator) writeMethod(sb *strings.Builder, m *CPPMethod, classe
 					sb.WriteString(fmt.Sprintf("for (auto i = 0; i < len_%s; ++i) {\n", *arg.Ident))
 					g.writeIndent(sb, 2)
 					if isObject {
+						if isClass(*arg.Type, classes) {
+							namespace = *classes[*arg.Type].NameSpace
+						}
 						sb.WriteString(fmt.Sprintf("if (!info[%d].As<Napi::Array>().Get(i).IsObject()) {\n", i))
 					} else {
 						sb.WriteString(fmt.Sprintf("if (!info[%d].As<Napi::Array>().Get(i).Is%s()) {\n", i, upper_caser.String(tsType[0:1])+tsType[1:]))
@@ -195,7 +200,6 @@ func (g *PackageGenerator) writeMethod(sb *strings.Builder, m *CPPMethod, classe
 			sb.WriteString(") {\n")
 			obj_name := ""
 			obj_type := ""
-			namespace := ""
 			for i, arg := range *m.Overloads[0] {
 				if v, ok := g.conf.MethodArgTransforms[*m.Ident][*arg.Ident]; ok && strings.Contains(v, "/arg_") {
 					g.writeIndent(sb, 2)
@@ -276,6 +280,58 @@ func (g *PackageGenerator) writeMethod(sb *strings.Builder, m *CPPMethod, classe
 			g.writeIndent(sb, 1)
 			sb.WriteString("return env.Null();\n")
 		} else {
+			g.writeIndent(sb, 2)
+			sb.WriteString(fmt.Sprintf("%s::%s _res;\n", namespace, *m.Returns))
+			if v, ok := g.conf.MethodReturnTransforms[*m.Ident]; ok {
+				parsed_transform := strings.ReplaceAll(v, "/return/", "_res")
+				for i, arg := range *m.Overloads[0] {
+					fmtd_arg := ""
+					if isClass(*arg.Type, classes) {
+						fmtd_arg = fmt.Sprintf("*(%s->_%s)", *arg.Ident, lower_caser.String(*arg.Type))
+					} else {
+						fmtd_arg = *arg.Ident
+					}
+					parsed_transform = strings.ReplaceAll(parsed_transform, fmt.Sprintf("/arg_%d/", i), fmtd_arg)
+				}
+				transformed_lines := strings.Split(parsed_transform, "\n")
+				length := len(transformed_lines)
+				for i, line := range transformed_lines {
+					g.writeIndent(sb, 2)
+					if i == length-1 {
+						sb.WriteString(line)
+					} else {
+						sb.WriteString(fmt.Sprintf("%s\n", line))
+					}
+				}
+			} else {
+				g.writeIndent(sb, 2)
+				sb.WriteString(fmt.Sprintf("_res = %s::%s(", namespace, *m.Ident))
+				for i, arg := range *m.Overloads[0] {
+					if i > 0 {
+						sb.WriteString(", ")
+					}
+					if _, ok := g.conf.TypeMappings[*arg.Type]; ok {
+						sb.WriteString(fmt.Sprintf("%s::%s(%s)", namespace, *arg.Type, *arg.Ident))
+					} else if isClass(*arg.Type, classes) {
+						sb.WriteString(fmt.Sprintf("*(%s->_%s)", *arg.Ident, lower_caser.String(*arg.Type)))
+					} else {
+						sb.WriteString(*arg.Ident)
+					}
+				}
+				sb.WriteString(");\n")
+			}
+			if v, ok := g.conf.GlobalTypeOutTransforms[*m.Returns]; ok {
+				g.writeIndent(sb, 2)
+				sb.WriteString(strings.ReplaceAll(v, "/return/", "_res"))
+			}
+			g.writeIndent(sb, 2)
+			sb.WriteString(fmt.Sprintf("auto* out = new %s::%s(_res);\n", namespace, *m.Returns))
+			g.writeIndent(sb, 2)
+			sb.WriteString(fmt.Sprintf("auto wrapped = Napi::External<%s::%s>::New(env, out);\n", namespace, *m.Returns))
+			g.writeIndent(sb, 2)
+			sb.WriteString(fmt.Sprintf("Napi::Value wrapped_out = %s::constructor->New({wrapped});\n", *m.Returns))
+			g.writeIndent(sb, 2)
+			sb.WriteString("return wrapped_out;\n")
 
 		}
 	} else {
