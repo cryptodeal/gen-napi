fd50fa6257d459cd1ffee8715505940db20b21ef
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-12-30 10:40:58 -0600

    attempt to fix `gorelease` build action

--- tensor.cjs
+++ tensor.cjs
@@ -4,6 +4,20 @@ class Tensor {
   constructor(t) {
     this.addon = new _Tensor(t)
   }
+
+  get ndim() {
+    return this.addon.ndim()
+  }
+
+  copy() {
+    return new Tensor(this.addon.copy())
+  }
+
+  get elements() {
+    return this.addon.elements()
+  }
 }
 
 const test = new Tensor(new Float32Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+console.log(test.elements)
+console.log(test.copy().ndim)
