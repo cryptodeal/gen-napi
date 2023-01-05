600417d5b6783438e903b61953aab9084bfbd785
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2023-01-05 13:28:37 -0600

    add test to repro

---
+++ bun_repro.cjs
@@ -0,0 +1,27 @@
+const { Tensor } = require('./test/js/tensor.cjs')
+const sm = require('./test/js/index.cjs')
+sm.init()
+function genRand() {
+  const out = new Float32Array(128)
+  for (let i = 0; i < 128; ++i) {
+    out[i] = Math.random()
+  }
+  return out
+}
+
+const test = async () => {
+  const t0 = performance.now() / 1e3
+  let m = 0
+  for (let i = 0; i < 10000; ++i) {
+    // console.log('bytes: ', Number(sm.bytesUsed()))
+    const a = sm.rand([128])
+    const b = new Tensor(genRand())
+    m += a.add(b).mean([], false).toFloat32Scalar()
+  }
+  const t1 = performance.now() / 1e3
+  console.log(t1 - t0, 'seconds to calculate', m)
+  m = null
+  await new Promise((r) => setTimeout(r, 2000))
+  console.log('bytes: ', Number(sm.bytesUsed()))
+}
+await test()
